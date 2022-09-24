0ed30230a600f121dcc2f12cf423311728da4937
RomanStets <roma22stets@gmail.com>
2022-09-24 14:28:47 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+
+const imagesMarkap = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkap);
+
+function createGalleryMarkup(_item) {
+  return _item
+    .map(({ preview, original, description }) => {
+      return `
+           <div class="gallery__item">
+                <a class="gallery__link" href="${original}"> 
+                <img 
+                    class="gallery__image"
+                    src="${preview}" 
+                    alt="${description}"
+                />                    
+                </a>
+            </div> `;
+    })
+    .join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.9,
+});
